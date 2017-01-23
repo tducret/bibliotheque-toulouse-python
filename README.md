431d76f7574fa4e15a456f4d06905d012b9c131a
thibdct <thibault.ducret@gmail.com>
2017-01-23 23:47:26 +0100

    Complément README et setup.py

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-Ce module permet d'interroger le catalogue des bibliothèques de Toulouse
+bibliothequetoulouse
+====================================
+
+## Description
+
+Ce package permet d'interroger très simplement le catalogue des [bibliothèques de Toulouse](http://bibliotheque.toulouse.fr).
+ 
+## Pré-requis
+
+ - Python 2.7
+
+## Installation
+
+    $ pip install -U bibliothequetoulouse
+
+## Utilisation
+
+Cet exemple permet de récupérer les informations sur les exemplaires d'un livre dans les bibliothèques de Toulouse, et de les afficher au format JSON :
+
+```python
+import bibliothequetoulouse as bib
+import json
+
+liste_resultats = bib.rechercher(titre=u"le meilleur des mondes", auteur=u"aldous huxley")
+print json.dumps(liste_resultats, ensure_ascii=False, indent=4, sort_keys=True)
+print "%d resultat(s)" % len(liste_resultats)
+``
--- setup.py
+++ setup.py
@@ -1,14 +1,24 @@
 # -*- coding: utf-8 -*-
 from distutils.core import setup
+
+requirements = [
+    'requests>=2.10.0',
+    'beautifulsoup4>=4.4.1'
+]
+
 setup(
   name = 'bibliothequetoulouse',
   packages = ['bibliothequetoulouse'],
   version = '0.1.0',
+  license='MIT',
+  platforms='Posix; MacOS X',
   description = 'Package pour interroger le catalogue des bibliothèques de Toulouse',
   author = 'Thibault Ducret',
   author_email = 'thibault.ducret@gmail.com',
   url = 'https://github.com/thibdct/bibliotheque-toulouse-python',
   download_url = 'https://github.com/thibdct/bibliotheque-toulouse-python/tarball/0.1.0', # I'll explain this in a second
   keywords = ['api', 'bibliothèque', 'toulouse'],
-  classifiers = [],
+  setup_requires=requirements,
+  install_requires=requirements,
+  classifiers = ['Programming Language :: Python :: 2.7'],
 )
