4f7a91965267730fdaaa883be37cabdfc9b22f16
thibdct <thibault.ducret@gmail.com>
2017-02-05 00:25:34 +0100

    Ajout du deuxième paramètre pour parse_requirements

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 from pip.req import parse_requirements
 
-install_reqs = parse_requirements('requirements.txt')
+install_reqs = parse_requirements('requirements.txt', session='hack')
 requirements = [str(ir.req) for ir in install_reqs]
 
 setup(
