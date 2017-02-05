0c4e172b1889daf7974009300e63bca32d2d2769
thibdct <thibault.ducret@gmail.com>
2017-02-05 16:49:21 +0100

    Correction de l'oubli de màj du numéro de version dans setup.py

--- setup.py
+++ setup.py
@@ -2,20 +2,21 @@
 from setuptools import setup
 from pip.req import parse_requirements
 
+_VERSION = '0.1.3'
 install_reqs = parse_requirements('requirements.txt', session='hack')
 requirements = [str(ir.req) for ir in install_reqs]
 
 setup(
   name = 'bibliothequetoulouse',
   packages = ['bibliothequetoulouse'],
-  version = '0.1.2',
+  version = _VERSION,
   license='MIT',
   platforms='Posix; MacOS X',
   description = 'Package pour interroger le catalogue des bibliothèques de Toulouse',
   author = 'Thibault Ducret',
   author_email = 'thibault.ducret@gmail.com',
   url = 'https://github.com/thibdct/bibliotheque-toulouse-python',
-  download_url = 'https://github.com/thibdct/bibliotheque-toulouse-python/tarball/0.1.1', # I'll explain this in a second
+  download_url = 'https://github.com/thibdct/bibliotheque-toulouse-python/tarball/%s' % (_VERSION), # I'll explain this in a second
   keywords = ['api', 'bibliothèque', 'toulouse'],
   setup_requires=requirements,
   install_requires=requirements,
