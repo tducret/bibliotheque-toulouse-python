2afa404173cf77c54daf10bcbc7962ecfe5280e9
thibdct <thibault.ducret@gmail.com>
2017-02-15 12:26:00 +0100

    Amélioration des tests de couverture

--- test/test_client.py
+++ test/test_client.py
@@ -5,9 +5,28 @@ def test_recherche_classique():
     resultats = bib.rechercher(titre=u"le meilleur des mondes",
                                auteur=u"aldous huxley",
                                bibli_souhaitees=[u'Médiathèque José Cabanis'])
+    print("%d resultat(s)" % len(resultats))
+    print(resultats) # Sortie JSON
     assert len(resultats) > 0
 
-def test_recherche_un_seule_page_detaillee():
+def test_recherche_une_seule_page_detaillee():
     resultats = bib.rechercher(titre=u"L'homme sans argent",
                                auteur=u"Mark Boyle")
+    print(resultats) # Sortie JSON
     assert len(resultats) > 0
+
+def test_dispo_uniquement():
+    resultats = bib.rechercher(titre=u"le meilleur des mondes",
+                           auteur=u"aldous huxley",
+                           dispo_uniquement=True)
+    assert len(resultats) > 0
+
+def test_auteur_seulement():
+    resultats = bib.rechercher(auteur=u"aldous huxley")
+    assert len(resultats) > 0
+    
+def test_titre_seulement():
+    resultats = bib.rechercher(titre=u"le meilleur des mondes")
+    assert len(resultats) > 0
+    
+# TODO : Ajout de tests pour afficher la liste, obtenir des éléments : resultats[0] res['dispo'], res.dispo 
