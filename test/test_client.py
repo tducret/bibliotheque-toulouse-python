081626b235f97829e0dc9939ad710cc5160dcacf
thibdct <thibault.ducret@gmail.com>
2017-02-15 14:59:51 +0100

    Ajout de nouveaux cas de tests

--- test/test_client.py
+++ test/test_client.py
@@ -1,12 +1,45 @@
 # -*- coding: utf-8 -*-
 import bibliothequetoulouse as bib
 
+def afficher_liste_resultats(liste_resultats):
+    print("%d resultat(s)" % len(liste_resultats))
+    print(liste_resultats)
+    for resultat in liste_resultats:
+        afficher_resultat_detaille(resultat)
+
+def afficher_resultat_detaille(resultat):
+    print(resultat)
+    print(u"Affichage par __getattr__")
+    print(u"Titre : %s" % (resultat.titre))
+    print(u"Auteur : %s" % (resultat.auteur))
+    print(u"ISBN : %s" % (resultat.isbn))
+    print(u"Bibliotheque : %s" % (resultat.bibliotheque))
+    print(u"Cote : %s" % (resultat.cote))
+    print(u"Materiel : %s" % (resultat.materiel))
+    print(u"Localisation : %s" % (resultat.localisation))
+    print(u"Retour attendu : %s" % (resultat.retour_attendu))
+    print(u"URL permanente : %s" % (resultat.url_permanent))
+    print(u"Pertinence : %.2f" % (resultat.pertinence))
+    print(u"Disponibilité : %s" % (resultat.dispo))
+    print
+    print(u"Affichage par __getitem__")
+    print(u"Titre : %s" % (resultat['titre']))
+    print(u"Auteur : %s" % (resultat['auteur']))
+    print(u"ISBN : %s" % (resultat['isbn']))
+    print(u"Bibliotheque : %s" % (resultat['bibliotheque']))
+    print(u"Cote : %s" % (resultat['cote']))
+    print(u"Materiel : %s" % (resultat['materiel']))
+    print(u"Localisation : %s" % (resultat['localisation']))
+    print(u"Retour attendu : %s" % (resultat['retour_attendu']))
+    print(u"URL permanente : %s" % (resultat['url_permanent']))
+    print(u"Pertinence : %.2f" % (resultat['pertinence']))
+    print(u"Disponibilité : %s" % (resultat['dispo']))
+        
 def test_recherche_classique():
-    resultats = bib.rechercher(titre=u"le meilleur des mondes",
-                               auteur=u"aldous huxley",
-                               bibli_souhaitees=[u'Médiathèque José Cabanis'])
-    print("%d resultat(s)" % len(resultats))
-    print(resultats) # Sortie JSON
+    resultats = bib.rechercher(titre=u"Les raisins de la colère",
+                               auteur=u"John Steinbeck",
+                               bibli_souhaitees=[u'Saint-Cyprien'])
+    afficher_liste_resultats(resultats)
     assert len(resultats) > 0
 
 def test_recherche_une_seule_page_detaillee():
@@ -22,11 +55,22 @@ def test_dispo_uniquement():
     assert len(resultats) > 0
 
 def test_auteur_seulement():
-    resultats = bib.rechercher(auteur=u"aldous huxley")
+    resultats = bib.rechercher(auteur=u"cyprien iov")
     assert len(resultats) > 0
     
 def test_titre_seulement():
-    resultats = bib.rechercher(titre=u"le meilleur des mondes")
+    resultats = bib.rechercher(titre=u"Roger et ses humains")
     assert len(resultats) > 0
     
-# TODO : Ajout de tests pour afficher la liste, obtenir des éléments : resultats[0] res['dispo'], res.dispo 
+def test_aucun_resultat():
+    resultats = bib.rechercher(titre=u"fdsfkjsdlkfjds",
+                               auteur=u"klfjzelkfjelz")
+    assert len(resultats) == 0
+
+def test_avec_braille():
+    resultats_sans_braille = bib.rechercher(titre=u"le meilleur des mondes",
+                               auteur=u"aldous huxley", sauf_braille=True)
+    resultats_avec_braille = bib.rechercher(titre=u"le meilleur des mondes",
+                               auteur=u"aldous huxley", sauf_braille=False)
+
+    assert len(resultats_sans_braille) < len(resultats_avec_braille)
